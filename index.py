f7785e4391b41f0fb06076de9329bc0ed9b3dedd
eaker <eakermane@gmail.com>
2022-09-01 15:42:36 +0300

    requirements

--- index.py
+++ index.py
@@ -31,7 +31,7 @@ def get_library_page():
     return response
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET'])
 def login_page():
     response = render_template('login.html')
 
